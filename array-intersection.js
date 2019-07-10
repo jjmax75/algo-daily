b5e4528c84f189b08efba456884db76e328a1b9c
John Behan <jjmax75@users.noreply.github.com>
2019-07-10 18:23:44 +0100

    Update array-intersection.js
    if set doesn't exist or is not allowed

--- array-intersection.js
+++ array-intersection.js
@@ -6,3 +6,14 @@ function intersection(nums1, nums2) {
   const intersection = new Set(nums2.filter(num => nums1Set.has(num)));
   return [...intersection];
 }
+
+// if Set never existed
+function intersectionNoSet(nums1, nums2) {
+  const intersection = {};
+  for (let num of nums1) {
+    if(nums2.includes(num)) {
+      intersection[num] = 1;
+    }
+  }
+  return Object.keys(intersection).map(key => key);
+}
